b957b50fd9c0a5ce499069de7b799b2c827139be
Fabricio Silva <fabricionaweb@gmail.com>
2020-12-01 19:24:18 +0000

    solve day-01 part 2

--- src/day-01/index.js
+++ src/day-01/index.js
@@ -13,4 +13,19 @@ export function part1(list = input) {
   return 0
 }
 
+export function part2(list = input) {
+  for (let i of list) {
+    for (let j of list) {
+      for (let k of list) {
+        if (i + j + k === MATCH) {
+          return i * j * k
+        }
+      }
+    }
+  }
+
+  return 0
+}
+
 console.log("Part 1 answer:", part1())
+console.log("Part 2 answer:", part2())
--- src/day-01/index.spec.js
+++ src/day-01/index.spec.js
@@ -1,6 +1,6 @@
 import { suite } from "uvu"
 import * as assert from "uvu/assert"
-import { part1 } from "./index.js"
+import { part1, part2 } from "./index.js"
 
 const test = suite("Day 1: Report Repair")
 
@@ -8,14 +8,26 @@ test("should return 0", () => {
   assert.is(part1([]), 0)
 })
 
+// provided
 test("should return 514579", () => {
   const input = [1721, 979, 366, 299, 675, 1456]
   assert.is(part1(input), 514579)
 })
 
-test("should return 2019", () => {
-  const input = [2019, 1]
-  assert.is(part1(input), 2019)
+test("should return 4036", () => {
+  const input = [2018, 2]
+  assert.is(part1(input), 4036)
+})
+
+// provided
+test("should return 241861950", () => {
+  const input = [1721, 979, 366, 299, 675, 1456]
+  assert.is(part2(input), 241861950)
+})
+
+test("should return 8064", () => {
+  const input = [2016, 2, 2]
+  assert.is(part2(input), 8064)
 })
 
 test.run()
